c1f7e64c7203e974d51c93b64997c9b8c61d6fd0
nikon <nikon@ukr.net>
2023-02-16 00:13:06 +0200

    16/02/23

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,75 @@
+class BooksController < ApplicationController
+  before_action :get_author
+  before_action :set_book, only: %i[ show edit update destroy ]
+
+  # GET /books or /books.json
+  def index
+    @books = @author.books
+  end
+
+  # GET /books/1 or /books/1.json
+  def show 
+  end
+
+  # GET /books/new
+  def new
+    @book = @author.books.build
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books or /books.json
+  def create
+    @book = @author.books.build(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to author_books_url, notice: "Book was successfully created." }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /books/1 or /books/1.json
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to author_books_url, notice: "Book was successfully updated." }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1 or /books/1.json
+  def destroy
+    @book.destroy
+
+    respond_to do |format|
+      format.html { redirect_to author_books_url, notice: "Book was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def get_author
+      @author = Author.find(params[:author_id])
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = @author.books.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title, :year, :author_id)
+    end
+end
